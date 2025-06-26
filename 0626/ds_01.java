57f011fcf221a577e24272584d60f09fc324659b
youtang11 <jiown.loveu@gmail.com>
2025-06-26 12:31:19 +0800

    Update ds_01.java

--- 0626/ds_01.java
+++ 0626/ds_01.java
@@ -2,9 +2,13 @@ import java.util.*;
 public class ds_01 {
     public static void main(String[] args) {
         Scanner s=new Scanner(System.in);
+        System.out.print("a=");
         int a=s.nextInt();
+        System.out.print("b=");
         int b=s.nextInt();
-        int c=a+b;
-        
+        int num=a+b;
+        System.out.println("num="+num); 
+        //s.close();
     } 
 }
+
