0b3332df9b464f92577e630774c070fd6b5cc334
youtang11 <jiown.loveu@gmail.com>
2025-06-26 12:10:17 +0800

    add java

---
+++ 0626/ds_01.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class ds_01 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int b=s.nextInt();
+        int c=a+b;
+        
+    } 
+}
